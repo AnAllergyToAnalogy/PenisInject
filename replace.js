dd3d9d41f9b9290b63f0df8f1e9bbf14a9a66311
William Entriken <github.com@phor.net>
2018-05-29 12:32:46 -0400

    Minify using ES6 anonymous function

--- replace.js
+++ replace.js
@@ -1,1 +1,1 @@
-setInterval(function(){var e=document.querySelectorAll("*");e[Math.round(Math.random()*e.length)].innerText=String.fromCharCode(112,101,110,105,115)},1e4);
+setInterval(m=>{var e=document.querySelectorAll("*");e[m.floor(m.random()*e.length)].innerText=String.fromCharCode(112,101,110,105,115)},1e4,Math);
